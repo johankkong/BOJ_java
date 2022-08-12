70b5beab3025f6204bdd52d00b83d68b753d3dca
johan kong <johankkong@gmail.com>
2022-08-12 17:55:45 +0900

    0812 BOJ 2304 solving..

--- S2/_2304/Main.java
+++ S2/_2304/Main.java
@@ -8,7 +8,33 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int[] ground = new int[1000];
 		int num = sc.nextInt();
-		for(int i = 0; )
+		for(int i = 0; i < num; i++) {
+			ground[sc.nextInt()] = sc.nextInt();
+		}
+		
+	}
+	
+	static int calculation(int[] arr) {
+		//최대 높이 찾기
+		int max = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		//가장 좌측 최대 인덱스와 우측 최대 인덱스도 구한다.
+		int maxLeft = 1000;
+		int maxRight = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == max) {
+				maxLeft = Math.min(i, maxLeft);
+				maxRight = Math.max(i, maxRight);
+			}
+		}
+		//우측재귀함수
+		
+		//좌측재귀함수
+		
 	}
 
 }
