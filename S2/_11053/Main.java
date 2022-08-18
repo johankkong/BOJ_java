1a3bd9cc4548aaffa49f8f22919cc749bdd33339
johan kong <johankkong@gmail.com>
2022-08-18 13:17:16 +0900

    0818 BOJ 11053 solved

--- S2/_11053/Main.java
+++ S2/_11053/Main.java
@@ -4,29 +4,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-	static int result = 0;
-	static Stack<Integer> stack = new Stack<>();
-	static void increase(int[] arr, int start) {
-		int tmp = 0;
-		for(int i = start; i < arr.length; i++) {
-			if(!stack.isEmpty() && arr[i] > stack.peek()) tmp = arr[i];
-		}
-		if(!stack.isEmpty() && tmp == 0) {
-			if(stack.size() > result) result = stack.size();
-			return;
-		}
-		for(int i = start; i < arr.length; i++) {
-			if(stack.isEmpty()) {
-				stack.push(arr[i]);
-			}
-			else if(arr[i] > stack.peek()) {
-				stack.push(arr[i]);
-			}
-			increase(arr, i+1);
-			if(!stack.isEmpty())stack.pop();
-		}
-	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
@@ -34,7 +11,25 @@ public class Main {
 		for(int i = 0; i < num; i++) {
 			arr[i] = sc.nextInt();
 		}
-		increase(arr, 0);
-		System.out.println(result);
+		int[] dp = new int[num];  //각 칸에 대한 최대 수열 길이를 저장할 배열 생성
+		//수열이 1개라면 최대 수열 길이는 1
+		dp[0] = 1;
+		//모든칸에 대해 한칸씩 움직이며
+		for(int i = 1; i < num; i++) {
+			int max = 0;
+			//그 이전칸들 중
+			for(int j = 0; j < i; j++) {
+				//현재칸보다 작은 수를 가진 칸의 최대 수열길이를 구한다.
+				if(arr[i] > arr[j]) max = Math.max(max, dp[j]);
+			}
+			//그 최대 수열길이에 1을 더해 현재 dp에 저장한다.
+			dp[i] = max + 1;
+		}
+		//결과를 출력
+		int ans = 0;
+		for(int i = 0; i < num; i++) {   //dp배열 중 최대값
+			ans = Math.max(ans, dp[i]);
+		}
+		System.out.println(ans);
 	}
 }
