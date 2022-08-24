26a29de20ad447e447692b7504b3c3a2c43ceff0
johankkong <johankkong@gmail.com>
2022-08-24 19:36:44 +0900

    0824 BOJ 12865 solving...

--- G5/_12865/Main.java
+++ G5/_12865/Main.java
@@ -13,10 +13,12 @@ public class Main {
 			things[i][1] = sc.nextInt();  //가치
 		}
 		int[] dp = new int[total + 1];  //가능한 무게 크기의 dp배열
-		for(int i = 0; i < num; i++) {  //무게 별로 dp를 채워나간다.
-			for(int j = things[i][0]; j <= total; j++) {
-				//현재 무게에 채워져 있는 가치와 이번 무게를 더했을 때의 가치를 비교하여 더 큰 것을 넣는다.
-				dp[j] = Math.max(dp[j], dp[j - things[i][0]] + things[i][1]); 
+		for(int i = 0; i < total; i++) {  //dp배열을 채워나간다.
+			for(int j = 0; j < num; j++) {  //각 물건들에 대해
+				if(i - things[j][0] >= 0) {  //물건의 무게를 감당할 수 있는 경우에만
+								//해당칸에 들어가 있는 것과 새로운 물건의 가치를 더한 것 중 큰 것을 저장한다.
+					dp[i] = Math.max(dp[i], dp[i - things[j][0]] + things[j][1]);
+				}
 			}
 		}
 		int max = 0;
