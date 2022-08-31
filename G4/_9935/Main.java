19debcf03709fc014ff9f28022b10143e96cdaad
johankkong <johankkong@gmail.com>
2022-08-31 21:21:37 +0900

    0831 BOJ 1330 solved

--- G4/_9935/Main.java
+++ G4/_9935/Main.java
@@ -1,7 +1,35 @@
 package G4._9935;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		Queue<Character> text = new LinkedList<>();
+		Queue<Character> bomb = new LinkedList<>();
+		while(true) {
+			char curr = (char)in.read();
+			if(curr == 13) break;
+			text.offer(curr);
+		}
+		while(true) {
+			char curr = (char)in.read();
+			if(curr == 13) break;
+			bomb.offer(curr);
+		}
+		Queue<Character> tmp = new LinkedList<>();
+		do {
+			for(int i = 0; i < text.size(); i++) {
+				char curr = text.poll();
+				if(curr != bomb.peek()) text.offer(curr);
+				else {
+					
+				}
+			}
+		}
 	}
 }
