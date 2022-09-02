decfe77631c5b3c88d3c8262f7ee9da9f308ab11
johankkong <johankkong@gmail.com>
2022-09-02 23:13:44 +0900

    0902 BOJ 10998 solved

---
+++ B5/_10998/Main.java
@@ -0,0 +1,11 @@
+package B5._10998;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int result = sc.nextInt() * sc.nextInt();
+        System.out.println(result);
+    }
+}
