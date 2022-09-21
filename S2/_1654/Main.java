4bb82606ef7044d0728c80ec918813cf1effc55c
johankkong <johankkong@gmail.com>
2022-09-21 19:06:08 +0900

    0921 BOJ 1654 solved

--- S2/_1654/Main.java
+++ S2/_1654/Main.java
@@ -1,4 +1,4 @@
-package S2._1564;
+package S2._1654;
 
 import java.util.Scanner;
 
@@ -10,32 +10,28 @@ public class Main {
 		K = sc.nextInt();
 		N = sc.nextInt();
 		arr = new int[K];
+		int maxLen = 0;
 		for(int i = 0; i < K; i++) {
 			arr[i] =  sc.nextInt();
+			maxLen = Math.max(arr[i], maxLen);
 		}
-		
-		//잘린 랜선의 갯수
-		long cnt = 0;
-		//자를 랜선의 길이
-		int len = 0;
-		//랜선의 길이를 1씩 더해가며 갯수가 N보다 작아질때까지 진행
-		do {
-			cnt = 0;
-			len++;
-			//모든 랜선을 길이로 자른다.
-			for(int i = 0; i < K; i++) {
-				cnt += arr[i] / len;
-			}
-		} while(cnt >= N);
-		//위 반복문을 빠져나오면 갯수가 N보다 작은 첫 길이
-		//따라서 그 길이 - 1이 정답이다.
-		System.out.println(len - 1);
+		result = 0;
+		binaryS(1, maxLen);
+		System.out.println(result);
 	}
 	
+	static int result;
 	//이진 탐색으로 최대 길이를 찾아나간다.
 	static void binaryS(int left, int right) {
-		int mid = (left + right) / 2;
-		
+		int mid = (int)(((long)left + (long)right) / 2);
+		if(mid == left || mid == right) {
+			if(cutLen(right) < N) result = left;
+			else result = right;
+			return;
+		}
+		long midNum = cutLen(mid);
+		if(midNum < N) binaryS(left, mid);
+		if(midNum >= N) binaryS(mid, right);
 	}
 	
 	//해당 길이로 잘랐을 때 몇개의 랜선이 나오는 지..
