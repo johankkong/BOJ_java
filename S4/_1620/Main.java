4cf4aadfc2e443ce0ab785804a466d1b7b73970b
johankkong <johankkong@gmail.com>
2022-09-27 10:57:27 +0900

    0927 BOJ 1620 solved

--- S4/_1620/Main.java
+++ S4/_1620/Main.java
@@ -3,6 +3,8 @@ package S4._1620;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,21 +13,20 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		String[] pokemons = new String[N];
-		for (int i = 0; i < N; i++) {
-			pokemons[i] = in.readLine();
+		Map<String, Integer> pokemonsMap = new HashMap<>();
+		String[] pokemonsArr = new String[N + 1];
+		for (int i = 1; i <= N; i++) {
+			String pokemon = in.readLine();
+			pokemonsMap.put(pokemon, i);
+			pokemonsArr[i] = pokemon;
 		}
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
 			String quest = in.readLine();
 			if(quest.charAt(0) <= '9') {
-				sb.append(pokemons[Integer.parseInt(quest) - 1]).append("\n");
+				sb.append(pokemonsArr[Integer.parseInt(quest)]).append("\n");
 			} else {
-				for (int j = 0; j < N; j++) {
-					if(quest.equals(pokemons[j])) {
-						sb.append(j + 1).append("\n");
-					}
-				}
+				sb.append(pokemonsMap.get(quest)).append("\n");
 			}
 		}
 		System.out.println(sb);
