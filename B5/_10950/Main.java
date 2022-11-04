e9d2fce1b4454c5d2a532eee7a36fde5fd35f562
johan kong <johankkong@gmail.com>
2022-11-04 23:51:09 +0900

    1104 BOJ 10950 solved

---
+++ B5/_10950/Main.java
@@ -0,0 +1,15 @@
+package B5._10950;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a + b);
+		}
+	}
+}
