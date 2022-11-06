218515b2d6c9670d8e585a37386b04dbe5efb9c8
johan kong <johankkong@gmail.com>
2022-11-06 23:45:31 +0900

    1106 BOJ 10171 solved

---
+++ B5/_10171/Main.java
@@ -0,0 +1,10 @@
+package B5._10171;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\r\n"
+				+ " )  ( ')\r\n"
+				+ "(  /  )\r\n"
+				+ " \\(__)|");
+	}
+}
