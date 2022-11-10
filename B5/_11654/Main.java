247060197dd5efd4306eb22adab73b5384c8d41e
johan kong <johankkong@gmail.com>
2022-11-10 23:54:42 +0900

    1110 BOJ 11654 solved

---
+++ B5/_11654/Main.java
@@ -0,0 +1,12 @@
+package B5._11654;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		int ans = (int)(input.charAt(0));
+		System.out.println(ans);
+	}
+}
