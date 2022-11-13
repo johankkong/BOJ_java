5ebb44c3550c9a751afc95594e305789fa8f2b11
johan kong <johankkong@gmail.com>
2022-11-13 22:10:48 +0900

    1113 BOJ 1167 solved

--- G2/_1167/Main.java
+++ G2/_1167/Main.java
@@ -19,15 +19,38 @@ public class Main {
 		}
 		for (int i = 0; i < V; i++) {
 			StringTokenizer st = new StringTokenizer(in.readLine());
-			st.nextToken();
+			int start = Integer.parseInt(st.nextToken()) - 1;
 			int end = Integer.parseInt(st.nextToken()) - 1;
 			while(end != -2) {
 				int weight = Integer.parseInt(st.nextToken());
-				adjList[i].add(new int[]{end, weight});
+				adjList[start].add(new int[]{end, weight});
 				end = Integer.parseInt(st.nextToken()) - 1;
 			}
 		}
-
-
+		// 1. 한 정점에서 가장 먼 점을 구한다.
+		visited = new boolean[V];
+		result = new int[2];
+		dfs(0, 0);
+		// 2. 1번에서 구한 가장 먼 점에서 가장 먼 점을 구한다.
+		visited = new boolean[V];
+		dfs(result[0], 0);
+		// 3. 그 때의 거리가 트리의 지름이다.
+		System.out.println(result[1]);
+	}
+	static boolean[] visited;
+	static int[] result;
+	private static void dfs(int curr, int weight) {
+		visited[curr] = true;
+		for (int i = 0; i < adjList[curr].size(); i++) {
+			if(!visited[adjList[curr].get(i)[0]]) {
+				dfs(adjList[curr].get(i)[0], weight + adjList[curr].get(i)[1]);
+			}
+		}
+		visited[curr] = false;
+		if(result[1] < weight) {
+			result[0] = curr;
+			result[1] = weight;
+		}
 	}
 }
+
