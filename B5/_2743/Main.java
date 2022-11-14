4c054dd4f647b409db601a783b60930bf29672a0
johan kong <johankkong@gmail.com>
2022-11-14 23:03:44 +0900

    1114 BOJ 2743 solved

---
+++ B5/_2743/Main.java
@@ -0,0 +1,11 @@
+package B5._2743;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        System.out.println(text.length());
+    }
+}
